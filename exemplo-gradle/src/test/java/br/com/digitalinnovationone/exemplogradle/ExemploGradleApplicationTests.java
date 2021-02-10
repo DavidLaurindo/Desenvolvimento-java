da77a2503a9f49e850c89f87927304b7035b2d4e
David Laurindo <45413191+DavidLaurindo@users.noreply.github.com>
2021-02-10 16:34:13 -0300

    Spring boot Maven e Gradle

---
+++ exemplo-gradle/src/test/java/br/com/digitalinnovationone/exemplogradle/ExemploGradleApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.digitalinnovationone.exemplogradle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExemploGradleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
